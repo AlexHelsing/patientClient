5e78757fa697e8d27cd3013a9d591b90eac46fe8
Johan Sandgren <gussanjoba@student.gu.se>
2023-11-13 22:14:21 +0100

    search bar for map view design implemented

--- src/views/MapViewPage.vue
+++ src/views/MapViewPage.vue
@@ -1,11 +1,13 @@
-<template lang="">
-    <div>
-        mapview
-    </div>
+<template>
+    <mapViewSearchBar />
 </template>
-<script setup lang="ts">
 
+<script setup lang="ts">
+import mapViewSearchBar from '../components/mapViewSearchBar.vue';
 </script>
+
+
+
 <style lang="">
     
 </style>
