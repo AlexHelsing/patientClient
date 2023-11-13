15b364ec00310005e3ec3b64e80310199f7f780f
Johan Sandgren <gussanjoba@student.gu.se>
2023-11-13 21:42:11 +0100

    navbar completely responsive with hamburger button

--- src/views/DashboardPage.vue
+++ src/views/DashboardPage.vue
@@ -1,31 +1,12 @@
 <template>
     <div class="min-h-screen text-gray-900 bg-blue-200">
-        <header class="bg-white shadow">
-            <div class="max-w-8xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
-                <div class="flex gap-20 items-center">
-                    <span class="flex gap-2 items-center">
-                        <img class="h-12 w-12" src="../assets/tooth.svg" alt="tooth logo">
-                        <h1 class="text-3xl font-bold">NAME</h1>
-                    </span>
-            
-                    <nav class="flex gap-8">
-                        <h1 class="text-blue-600 hover:text-blue-700 font-semibold">Appointments</h1>
-                        <h1 class="text-blue-600 hover:text-blue-700 font-semibold">Dentistries</h1>
-                        <h1 class="text-blue-600 hover:text-blue-700 font-semibold">Payments</h1>
-                        <h1 class="text-blue-600 hover:text-blue-700 font-semibold">Settings</h1>
-                    </nav>
-                </div>
-           
-                <div class="flex gap-4 items-center">
-                    <span class="rounded-full bg-purple-600 h-12 w-12" alt="" />
-                </div>
-            </div>
-        </header>
+        <Navbar />
+       
 
     </div>
 </template>
 <script setup lang="ts">
-
+import Navbar from '../components/NavBar.vue'
 
 </script>
 <style lang="">
