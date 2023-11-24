fe57fe452404761ad65af7eaf053fca5b00a8f37
Johan Sandgren <gussanjoba@student.gu.se>
2023-11-24 20:37:19 +0100

    Showcase users location functionality, marker on map

--- src/views/MapViewPage.vue
+++ src/views/MapViewPage.vue
@@ -10,7 +10,6 @@
                 <h1 class="text-2xl font-bold">Make an appointment </h1>
 
                 <div class="flex w-full items-center gap-10 ">
-                    <!-- Adjusted search input container -->
                     <div class="relative flex-grow mr-3 md:w-3/5">
                         <input type="text" v-model="adressInput" placeholder="Search by city, postal code or street"
                             class="w-full pl-3 pr-10 py-3 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" />
@@ -27,7 +26,7 @@
                 </div>
 
                 <div class="flex justify-between">
-                    <button @click="usingCurrentLocation = true" type="button"
+                    <button @click="getUserLocation" type="button"
                         class="text-white bg-blue-700 hover:bg-blue-800  focus:outline-none active:outline-none  rounded-xl font-semibold text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600  inline-flex items-center">
                         <svg v-if="usingCurrentLocation" aria-hidden="true" role="status"
                             class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none"
@@ -39,7 +38,7 @@
                                 d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
                                 fill="currentColor" />
                         </svg>
-                        {{ usingCurrentLocation ? 'Finding your location' : 'Use my current location' }}
+                        {{ usingCurrentLocation ? 'Using your location ' : 'Use my current location' }}
                     </button>
 
                     <div class="flex gap-2 items-center">
@@ -88,6 +87,12 @@
                     <l-popup :content="`<h1>Chalmers University of Technology</h1>`" :lat-lng="campusMarker">
                     </l-popup>
                 </l-marker> -->
+                <l-marker v-if="usingCurrentLocation" :lat-lng="usersLocation">
+                    <l-icon icon-url="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png"
+                        :icon-size="[40, 40]" :icon-anchor="[20, 40]"></l-icon>
+                    <l-popup content="You are here" :lat-lng="usersLocation" opened>
+                    </l-popup>
+                </l-marker>
 
                 <l-marker @click="handleMarkerClick(dentistry)" v-for="dentistry in dentistries" :key="dentistry.id"
                     :lat-lng="dentistry.coordinates">
@@ -111,13 +116,12 @@ import Paginator from 'primevue/paginator';
 import 'primevue/resources/themes/lara-light-teal/theme.css';
 import DentistryListItem from '../components/DentistryListItem.vue';
 import dentistryData from "../mockupdata";
-import { LMap, LTileLayer, LMarker, LPopup } from '@vue-leaflet/vue-leaflet';
+import { LMap, LTileLayer, LMarker, LPopup, LIcon } from '@vue-leaflet/vue-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { MagnifyingGlassIcon } from '@heroicons/vue/24/outline';
 const adressInput = ref(null);
 const DateInput = ref(null);
 const usingCurrentLocation = ref(false);
-// create a variable that has SearchInput type, and is a ref, dont use time as a variable name
 const showingSearchResults = ref({ time: 'All' } as { time: SearchInput });
 
 function handleChange(newValue: SearchInput) {
@@ -127,6 +131,27 @@ function handleChange(newValue: SearchInput) {
 
 
 type SearchInput = 'All' | 'Morning' | 'Afternoon';
+
+
+const usersLocation = ref({ lat: 0, lng: 0 });
+
+async function getUserLocation() {
+    navigator.geolocation.getCurrentPosition(async (position) => {
+        usersLocation.value.lat = position.coords.latitude;
+        usersLocation.value.lng = position.coords.longitude;
+        usingCurrentLocation.value = true;
+
+        // Update the center and zoom
+        center.value = [usersLocation.value.lat, usersLocation.value.lng];
+        // wait 0.5 seconds and then zoom
+        await new Promise(r => setTimeout(r, 500));
+        zoom.value = 16;
+
+
+    });
+}
+
+
 
 
 const dentistries = ref(dentistryData);
