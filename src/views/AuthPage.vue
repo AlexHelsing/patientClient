1ab72907040e2088feee6edb7d9c399bd3fe8e46
Johan Sandgren <gussanjoba@student.gu.se>
2024-01-07 22:01:40 +0100

    Add form components and dependencies

--- src/views/AuthPage.vue
+++ src/views/AuthPage.vue
@@ -13,9 +13,9 @@ const toggleSignUpForm = () => {
 
 
 <template>
-    <div class="min-h-screen flex text-black bg-gray-200">
+    <div class="min-h-screen flex text-black bg-gray-200 dark:bg-gray-800 dark:text-gray-50">
         <div
-            class="bg-gray-50 flex-[3] flex flex-col px-6 space-y-7 shadow-xl  border justify-center rounded-tr-xl rounded-br-xl">
+            class="bg-gray-50 dark:bg-gray-900 flex-[3] flex flex-col px-6 space-y-7 shadow-xl dark:border-none dark:rounded-none border justify-center rounded-tr-xl rounded-br-xl">
             <span class="flex items-center gap-2 justify-center">
                 <img class="h-12 w-12 " src="../assets/tooth.svg" alt="tooth logo">
                 <h1 class="text-4xl bg-blend-color-dodge font-bold">Patient Client</h1>
@@ -32,11 +32,11 @@ const toggleSignUpForm = () => {
                     <div class="w-full border-t border-gray-300"></div>
                 </div>
                 <div class="relative flex justify-center text-sm">
-                    <span class="px-2 bg-gray-50 text-gray-500">OR</span>
+                    <span class="px-2 bg-gray-50 dark:bg-gray-900 dark:text-gray-50 text-gray-500">OR</span>
                 </div>
             </div>
             <button @click="toggleSignUpForm"
-                class="w-full text-gray-700 hover:text-white flex justify-center py-3 px-4 border rounded-md shadow-sm text-sm font-medium  border-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
+                class="w-full dark:text-white text-gray-700 dark:hover:bg-gray-800 hover:text-white flex justify-center py-3 px-4 dark:bg-gray-700  border rounded-md shadow-sm text-sm font-medium dark:border-gray-400  border-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500">
                 {{ showSignUpForm ? 'Back to Login' : 'Signup now' }}</button>
 
         </div>
